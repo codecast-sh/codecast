20b1638bb507696af86191ae10693791cc84aa02
Ashot Petrosian <ashot@almostcandid.com>
2026-02-19 17:08:06 -0800

    chore(cli): bump version to 1.0.41

--- packages/cli/src/update.ts
+++ packages/cli/src/update.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import * as os from "os";
 import { execSync, spawn } from "child_process";
 
-const VERSION = "1.0.40";
+const VERSION = "1.0.41";
 const MEMORY_VERSION = "3";
 const TASK_VERSION = "1";
 const LATEST_URL = "https://dl.codecast.sh/latest.json";
