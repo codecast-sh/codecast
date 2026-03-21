10a45210b092835e759ba636ef1d2b9954465ad4
Ashot Petrosian <ashot@almostcandid.com>
2026-03-21 13:51:49 -0500

    chore(cli): sync bundled daemon version

--- packages/cli/src/daemon.js
+++ packages/cli/src/daemon.js
@@ -10910,7 +10910,7 @@ async function handlePermissionRequest(syncService, conversationId, sessionId, p
 import * as fs10 from "fs";
 import * as path10 from "path";
 import * as os from "os";
-var VERSION = "1.0.87";
+var VERSION = "1.0.88";
 var LATEST_URL = "https://dl.codecast.sh/latest.json";
 var UPDATE_CHECK_INTERVAL = 24 * 60 * 60 * 1000;
 var CONFIG_DIR3 = process.env.HOME + "/.codecast";
