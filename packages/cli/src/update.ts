127fb2404df3f89482bcf7297fde523e807a7fe0
Ashot Petrosian <ashot@almostcandid.com>
2026-03-26 17:09:11 -0500

    chore(cli): bump to v1.0.99

--- packages/cli/src/update.ts
+++ packages/cli/src/update.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
 
-const VERSION = "1.0.98";
+const VERSION = "1.0.99";
 const MEMORY_VERSION = "3";
 const TASK_VERSION = "1";
 const WORK_VERSION = "5";
