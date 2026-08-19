12664b1c7f3db8c6ce135812108950eee3df3dc0
Samvit Ramadurgam <rsamvit@gmail.com>
2026-08-19 16:18:41 -0700

    codecast wip snapshot
    codecast-branch: main

--- packages/web/app/tasks/page.tsx
+++ packages/web/app/tasks/page.tsx
@@ -1410,6 +1410,7 @@ export function TaskListContent({ projectId }: { projectId?: string } = {}) {
     <GenericListView<TaskItem>
           activeItemId={(projectId ? params?.taskId : params?.id) as string | undefined}
           paletteTargetType="task"
+          getComposeRef={(t) => t.short_id}
           title={projectId ? "Project tasks" : "Tasks"}
           tabs={[
             // Two presets, then the statuses they are made of. Each preset
